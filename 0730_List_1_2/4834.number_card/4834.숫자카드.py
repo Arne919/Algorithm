4bec32094e31479137dbd2d7a9b4a35ea15d3ddd
kanghyekyeong <ghgghg96@naver.com>
2024-07-30 09:04:19 +0900

    9시

---
+++ 0730_List_1_2/4834.number_card/4834.숫자카드.py
@@ -0,0 +1,16 @@
+'''
+3
+5
+49679
+5
+08271
+10
+7797946543
+'''
+T = int(input()) #테스트케이스 개수
+for test_case in range(1, T+1):
+    N = int(input())
+    a = list(map(int, input().split()))
+
+
+# print(f'#{test_case} ')
