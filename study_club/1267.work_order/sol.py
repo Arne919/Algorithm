b14581908cf404b52c03da3df4a4f3f7f962066d
KangHyeKyeong <ghgghg96@naver.com>
2024-08-09 21:57:25 +0900

    8/9_21:57

--- study_club/1267.work_order/sol.py
+++ study_club/1267.work_order/sol.py
@@ -1,18 +1,29 @@
 import sys
 sys.stdin = open('input.txt')
 
-def DFS(adjl):
-    visited = [0] * (V+1)
-    stack = []
-    result = []
-    visited[v2] = 1
-
 
 for tc in range(1, 11):
     V, E = map(int, input().split())
-    adjl = [[] for _ in range(V)]
-    arr = list(map(int, input().split))
+    adjl = [[] for _ in range(V+1)]
+    visited = [True for _ in range(V+1)]
+    arr = list(map(int, input().split()))
+    cnt = V
+
     for i in range(E):
-        v1, v2 = arr[i*2], arr[i*2+1]
-        adjl[v1].append(v2)
-    print(f'#{tc} {DFS(adjl)}')
+        a, b = arr[i*2], arr[i*2+1]
+        adjl[b].append(a)
+    result = []
+    while cnt:
+        for i in range(1, len(adjl)):
+            if visited[i]:
+                for j in adjl[i]:
+                    if visited[j]:
+                        break
+                else:
+                    visited[i] = False
+                    result.append(i)
+                    cnt -= 1
+
+
+    print(f'#{tc} ', end='')
+    print(*result)
