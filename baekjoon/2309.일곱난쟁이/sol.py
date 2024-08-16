40b0521b7767fb0a7caadf6e33af06e0a836050d
kanghyekyeong <ghgghg96@naver.com>
2024-08-16 17:10:25 +0900

    8/16_백준데이

--- baekjoon/2309.일곱난쟁이/sol.py
+++ baekjoon/2309.일곱난쟁이/sol.py
@@ -3,14 +3,13 @@ sys.stdin = open('input.txt')
 
 arr = list(int(input()) for _ in range(9))
 # print(arr)
-# sum(arr)
+total = sum(arr)
 # print(sum(arr))
 for i in range(len(arr)-1):
     for j in range(i+1,(len(arr))):
-        result = sum(arr)-arr[i]-arr[j]
-        if result == 100:
+        # result = total-arr[i]-arr[j]
+        if sum(arr)-arr[i]-arr[j] == 100:
             arr.remove(arr[i])
             arr.remove(arr[j])
             list.sort(arr)
-            print(arr, end = '')
-# print(arr)
+            print(*arr)
