6639860b0a06f146c32a965bd2873399dd44c7e2
KangHyeKyeong <ghgghg96@naver.com>
2024-08-18 17:46:41 +0900

    방배정

--- baekjoon/13300.방배정/sol.py
+++ baekjoon/13300.방배정/sol.py
@@ -2,20 +2,19 @@ import sys
 sys.stdin = open('input.txt')
 
 N, K = map(int,input().split())
-room = [[0,0] for _ in range(6)] # [여,남] * 6
-result = 0
+student = [[0,0] for _ in range(6)] # [여,남] * 6
+room = 0
 
 for i in range(N):
     S, Y = map(int,input().split())
     if S == 0:
-        room[Y-1][0] += 1
+        student[Y-1][0] += 1
     else:
-        room[Y-1][1] += 1
-print(room)
-# for grade in students:
-#     for num in grade:
-#         res+=num//K
-#         if num % K: # 만약 남은 인원이 있다면(나머지)
-#             res+=1 # 방이 하나더 필요
-    
-# print(res)
+        student[Y-1][1] += 1
+# print(student)
+for i in range(6):  # 학년
+    for j in range(2):  # 성별
+        room += student[i][j]//K
+        if student[i][j]%K != 0:    # 남은인원있으면 room +1
+            room += 1
+print(int(room))
