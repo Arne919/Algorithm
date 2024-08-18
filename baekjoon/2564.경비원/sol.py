de9e6483704f80619253c7278e7438196db8fe17
KangHyeKyeong <ghgghg96@naver.com>
2024-08-18 16:34:51 +0900

    경비원

--- baekjoon/2564.경비원/sol.py
+++ baekjoon/2564.경비원/sol.py
@@ -12,7 +12,6 @@ def cal(location,distance):
     elif location == 3: # 서
         return x+y+x+y-distance
 
-
 x, y = map(int, input().split())    # x:가로, y:세로
 store = int(input())                # 상점 갯수
 # 상점위치, 동근위치 입력받기
@@ -21,12 +20,11 @@ arr=[]
 for i in range(store+1):
     location, distance = locations[i][0], locations[i][1]
     arr.append(cal(location, distance))
-# 동근이 위치 계산
-DG_loc = arr[-1]
 
+DG_loc = arr[-1]        # 동근이 위치
 result = 0
 for i in range(store):
-    cal_dist = abs(arr[i]-DG_loc)
-    total = 2*(x+y)
-    result += min(cal_dist, total-cal_dist)
+    cal_dist = abs(arr[i]-DG_loc)   # 상점거리-동근 의 거리(절댓값)
+    total = 2*(x+y)     # 전체 길이
+    result += min(cal_dist, total-cal_dist) # 시계방향, 반시계방향중 최단거리 누적
 print(result)
