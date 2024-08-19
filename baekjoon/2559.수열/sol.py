236014d5ad1f3d51fa62adab9902489945032e94
KangHyeKyeong <ghgghg96@naver.com>
2024-08-19 23:42:32 +0900

    수열시간초과 다시함

--- baekjoon/2559.수열/sol.py
+++ baekjoon/2559.수열/sol.py
@@ -1,11 +1,23 @@
 import sys
 sys.stdin = open('input.txt')
 
-result = []
 N, K = map(int, input().split())
 arr = list(map(int, input().split()))
 
-for i in range(N - K + 1):
-    result.append(sum(arr[i:i+K]))
-    
+result = []
+result.append(sum(arr[:K]))
+
+for i in range(N-K):
+    result.append(result[i] - arr[i] + arr[K+i])
+
 print(max(result))
+
+# ㅅ_ㅂ 시간초과데스
+# result = []
+# N, K = map(int, input().split())
+# arr = list(map(int, input().split()))
+
+# for i in range(N - K + 1):
+#     result.append(sum(arr[i:i+K]))
+
+# print(max(result))
