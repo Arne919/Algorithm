053d93a54e4c9a247923eede354fb1123403270a
KangHyeKyeong <ghgghg96@naver.com>
2024-08-28 22:04:54 +0900

    .

--- 5w_(240826~240830)/240827_Tree_1/1231_중위순회/sol.py
+++ 5w_(240826~240830)/240827_Tree_1/1231_중위순회/sol.py
@@ -1,3 +1,33 @@
 import sys
 sys.stdin = open('input.txt')
 
+# 중위 순회
+def in_order(v):
+    if v:
+        # 왼쪽 자식
+        in_order(left[v])
+        result.append(tree[v])
+        # print(tree[v], end='')  # visit(v)
+        # 오른쪽 자식
+        in_order(right[v])
+
+for tc in range(1, 11):
+    N = int(input())    #정점 개수
+    tree = [0]*(N+1)
+    left = [0]*(N+1)            # 왼쪽 자식 번호를 저장할 리스트
+    right = [0] * (N + 1)       # 오른쪽 자식 번호를 저장할 리스트
+    result =[]
+    for _ in range(N):
+        node = input().split()
+        tree[int(node[0])] = node[1]
+        # 왼쪽 자식이 있다면
+        if len(node) >= 3:
+            left[int(node[0])] = int(node[2])
+        # 왼쪽 자식있고 오른쪽 자식도 있음
+        if len(node) == 4:
+            right[int(node[0])] = int(node[3])
+    in_order(1)
+    print(f'#{tc} {"".join(result)}')
+    # print(f'#{tc} ', end='')
+    # in_order(1)
+    # print()
--- 5w_(240826~240830)/240827_Tree_1/5174_서브트리/sol.py
+++ 5w_(240826~240830)/240827_Tree_1/5174_서브트리/sol.py
@@ -1,3 +1,27 @@
 import sys
 sys.stdin = open('input.txt')
 
+def search(node):
+    global cnt
+    if node:
+        cnt+=1
+        search(tree[node][0])
+        search(tree[node][1])
+
+T = int(input())
+for tc in range(1, T + 1):
+    E, N = map(int, input().split())
+    arr = list(map(int, input().split()))
+    V = E + 2
+    tree = [[0, 0] for _ in range(V)]
+
+    for i in range(E):
+        parent = arr[i*2]
+        child = arr[i*2+1]
+        if tree[parent][0] == 0:
+            tree[parent][0] = child
+        else:
+            tree[parent][1] = child
+    cnt = 0
+    search(N)
+    print(f'#{tc} {cnt}')
