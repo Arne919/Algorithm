fc17b7b772ed79faa59b1e45a0b9dcf14f892b8a
강혜경 <ghgghg96@naver.com>
2024-09-10 16:54:37 +0900

    그룹나누기

--- 7w_(240909~240913)/240910_그래프기본/5247.연산/sol.py
+++ 7w_(240909~240913)/240910_그래프기본/5247.연산/sol.py
@@ -1,2 +1,34 @@
 import sys
 sys.stdin = open('input.txt')
+
+from collections import deque
+
+# bfs탐색
+def solve(num):
+    global rst
+    # print(nums)
+    Q = deque()
+    # Q에는 현재 숫자와 카운트 해줄 숫자를 튜플로 넣어줌
+    Q.append((num, 0))
+    # 현재 숫자 방문 체크
+    visit.add(num)
+    while Q:
+        now, cnt = Q.popleft()
+        if now == M:
+            rst = min(rst, cnt)
+            return
+        # 갈 수 있는 경로는 4가지
+        tmp = [now+1, now-1, now*2, now-10]
+        for val in tmp:
+            # 방문하지 않았고, 제약조건의 숫자 범위라면 bfs탐색
+            if val not in visit and 0 < val <= 1000000:
+                visit.add(val)
+                Q.append((val, cnt+1))
+
+for tc in range(int(input())):
+    N, M = map(int, input().split())
+
+    visit = set()
+    rst = 987654321
+    solve(N)
+    print(f'#{tc+1}', rst)
--- 7w_(240909~240913)/240910_그래프기본/5248.그룹나누기/sol.py
+++ 7w_(240909~240913)/240910_그래프기본/5248.그룹나누기/sol.py
@@ -1,2 +1,28 @@
 import sys
 sys.stdin = open('input.txt')
+
+def dfs(v):
+    visited[v] = 1  # 방문 표시
+    for nxt in graph[v]:    # 정점에서 연결된 정점들 순회
+        if visited[nxt]:    # 방문하지 않은 정점들 선택
+            continue
+        dfs(nxt)
+
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    arr = list(map(int, input().split()))
+    graph = [[] for _ in range(N+1)]    # 1 ~ N번
+    for i in range(M):      # 무방향(쌍방) 그래프 연결 상태
+        v1 = arr[i*2]
+        v2 = arr[i*2+1]
+        graph[v1].append(v2)
+        graph[v2].append(v1)
+    visited = [0 for _ in range(N+1)]
+    cnt = 0
+    for i in range(1, N+1):
+        if visited[i]:  # 방문확인
+            continue
+        dfs(i)          # 연결 요소 모두 방문 표시
+        cnt += 1        # 연결 요소 개수 +1
+    print(f'#{tc} {cnt}')
